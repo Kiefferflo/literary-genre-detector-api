bf61843ec80c421b9fcc84f959bab521d0e2445b
Kiefferflo <kiefferflo@cy-tech.fr>
2023-02-22 13:29:49 +0100

    change to function's names

--- main.py
+++ main.py
@@ -3,14 +3,29 @@ from modelIA import *
 
 app = FastAPI()
 
-@app.post("/api/predict/summary")
+@app.get("/api/predict/summary")
 async def predictFromSummary(summary: str):
-    return predictWithSummary(summary)
+    fileName_Model = "Model/predictWithSummary_TreeClassifier"
+    return predictWithsummary_TreeClassifier_Predict(fileName_Model,summary)
 
-@app.post("/api/predict/title")
+@app.post("/api/train/summary")
+async def predictFromSummary():
+    fileName_Data = "Données/BooksDataSet.csv"
+    predictWithSummary_TreeClassifier_Model(fileName_Data)
+    return "Success"
+
+@app.get("/api/predict/title")
 async def predictFromTitle(title: str):
-    return predictWithTitle(title)
+    fileName_Model = "Model/predictWithTitle_TreeClassifier"
+    return predictWithTitle_TreeClassifier_Predict(fileName_Model,title)
 
-@app.post("/api/predict/author")
+@app.post("/api/train/title")
+async def predictFromTitle():
+    fileName_Data = "Données/BooksDataSet.csv"
+    predictWithTitle_TreeClassifier_Model(fileName_Data)
+    return "Success"
+
+@app.get("/api/predict/author")
 async def predictFromAuthor(author: str):
-    return predictWithAuthor(author)
+    nomFichier = 'Données/Author_generes.csv'
+    return predictWithAuthor(author,nomFichier)
