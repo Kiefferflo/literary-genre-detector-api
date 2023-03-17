4a76336cba535ab640b7eb59a5f9b8b69731eefa
Kiefferflo <kiefferflo@cy-tech.fr>
2023-03-17 14:25:07 +0100

    feature: CROS handling

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.modelIA import *
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/api/predict/summary")
 async def predictFromSummary(summary: str):
