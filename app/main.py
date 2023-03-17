16aa9fafd6083fc16fb23302d20614984e0e1f91
Kiefferflo <kiefferflo@cy-tech.fr>
2023-03-17 11:53:20 +0100

    fix: get to post

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.modelIA import *
 
 app = FastAPI()
 
-@app.get("/api/predict/summary")
+@app.post("/api/predict/summary")
 async def predictFromSummary(summary: str):
     fileName_Model = "app/Model/predictWithSummary_TreeClassifier"
     return predictWithsummary_TreeClassifier_Predict(fileName_Model,summary)
@@ -14,7 +14,7 @@ async def predictFromSummary():
     predictWithSummary_TreeClassifier_Model(fileName_Data)
     return "Success"
 
-@app.get("/api/predict/title")
+@app.post("/api/predict/title")
 async def predictFromTitle(title: str):
     fileName_Model = "app/Model/predictWithTitle_TreeClassifier"
     return predictWithTitle_TreeClassifier_Predict(fileName_Model,title)
@@ -25,7 +25,7 @@ async def predictFromTitle():
     predictWithTitle_TreeClassifier_Model(fileName_Data)
     return "Success"
 
-@app.get("/api/predict/author")
+@app.post("/api/predict/author")
 async def predictFromAuthor(author: str):
     nomFichier = 'app/Données/Author_generes.csv'
     return predictWithAuthor(author,nomFichier)
